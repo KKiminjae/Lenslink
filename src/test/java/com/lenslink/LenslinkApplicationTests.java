ada3ab7ae2401f05db655592dbbb11d4631cda10
KKiminjae <jason471566@gmail.com>
2026-08-07 17:08:06 +0900

    test: use Testcontainers for MySQL tests

--- src/test/java/com/lenslink/LenslinkApplicationTests.java
+++ src/test/java/com/lenslink/LenslinkApplicationTests.java
@@ -2,11 +2,24 @@ package com.lenslink;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class LenslinkApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mysql =
+			new MySQLContainer<>("mysql:8.4")
+					.withDatabaseName("lenslink_test")
+					.withUsername("test")
+					.withPassword("test");
 
 	@Test
 	void contextLoads() {
